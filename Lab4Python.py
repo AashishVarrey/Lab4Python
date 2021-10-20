fb6e5dbc532674044d4c1a13f1a75d40566f5a78
Aashish Varrey <91568763+AashishVarrey@users.noreply.github.com>
2021-10-20 03:17:50 +0000

    edits

--- Lab4Python.py
+++ Lab4Python.py
@@ -15,9 +15,19 @@ with open('Lab4.txt', 'w') as f:
 print('Content-type: text/html\n\n')
 print('<html>')
 print('<form action="/cgi-bin/Lab4Python.py" method="POST">')
-print('<input type = "radio" name="option" value="red"> Red Led <br>')
-print('<input type = "radio" name="option" value="yellow"> Yellow Led <br>')
-print('<input type = "radio" name="option" value="blue"> Blue Led <br>')
+if s1 == "red":
+  print('<input type = "radio" name="option" value="red" checked> Red Led <br>')
+  print('<input type = "radio" name="option" value="yellow"> Yellow Led <br>')
+  print('<input type = "radio" name="option" value="blue"> Blue Led <br>')
+elif s1 == "yellow":
+  print('<input type = "radio" name="option" value="red"> Red Led <br>')
+  print('<input type = "radio" name="option" value="yellow" checked> Yellow Led <br>')
+  print('<input type = "radio" name="option" value="blue"> Blue Led <br>')
+else:
+  print('<input type = "radio" name="option" value="red"> Red Led <br>')
+  print('<input type = "radio" name="option" value="yellow"> Yellow Led <br>')
+  print('<input type = "radio" name="option" value="blue" checked> Blue Led <br>')
+
 print('<input type = "range" name="ledvalue" min="0" max="100" value="%s"> <br>' %s2)
 print('<input type = "submit" value="Send Changes Over">')
 print('</form>')
